d35bf467807ba9b7acd7eec5f3ed2fb3dd21f5ed
cazuho <cakumubom9210@gmail.com>
2025-03-16 15:06:06 +0900

    8章ニュースにssrを使用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,9 @@ import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination";
 import SearchField from "../_components/SearchField";
 
+// キャッシュを使用せず毎回オリジナルサーバーにアクセス
+export const revalidate = 0;
+
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
